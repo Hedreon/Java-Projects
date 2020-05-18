27a7fc85cd394b57ca973e85f16857aa4a41cf29
Hedreon <39467046+Hedreon@users.noreply.github.com>
2020-05-18 18:52:44 +0100

    Create Karl.java

---
+++ Karl/src/Karl.java
@@ -0,0 +1,8 @@
+// This is the very first java program I made.
+// Preview: https://gyazo.com/df76a279665f605e95d725ad57ac36e2
+
+public class Karl {
+    public static void main(String[] args) {
+        System.out.println("Congratulations, Karl!");
+    }
+}
